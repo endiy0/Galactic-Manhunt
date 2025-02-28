05c238c50dfcee29435d1ad9aec784b3cc6c8b49
endiy0 <steve060401@gmail.com>
2025-02-28 14:17:42 +0900

    v0.0.2-a
    Planet.cs Constructor added
    Item.cs Constructor added

--- Server test/Item.cs
+++ Server test/Item.cs
@@ -11,9 +11,39 @@ namespace Server_test
     {
         Resource Itemtype;
         string Name;
-        public double mass; // 질량, 단위: kg
-
+        public double mass; //질량, 단위: kg
+        public static int ResourceCount = 10;
+        public static int AirCount = 3;
+        public static int CompountCount = 3;
+        public static int MineralCount = 2;
+        public static int OrganicMatterCount = 1;
         // TODO: Resource종류를 바꿀때마다 이름도 바꾸는 생성자 구현
+
+        public Item(Resource ITEMTYPE, double MASS)
+        {
+            Itemtype = ITEMTYPE;
+            mass = MASS;
+            if (Itemtype == Resource.Hydrogen)
+                Name = "수소";
+            else if (Itemtype == Resource.Nitrogen)
+                Name = "질소";
+            else if (Itemtype == Resource.Oxygen)
+                Name = "산소";
+            else if (Itemtype == Resource.Peroxide)
+                Name = "퍼옥사이드";
+            else if (Itemtype == Resource.Hydrazine)
+                Name = "하이드라진";
+            else if (Itemtype == Resource.Epsilon)
+                Name = "엑실론";
+            else if (Itemtype == Resource.Epsilon_crystal)
+                Name = "엑실론 크리스탈";
+            else if (Itemtype == Resource.Water)
+                Name = "물";
+            else if (Itemtype == Resource.Seed)
+                Name = "씨앗";
+            else if (Itemtype == Resource.Chrono)
+                Name = "크로노";
+        }
 
         public Resource GetItemType()
         {
@@ -26,19 +56,24 @@ namespace Server_test
         }
     }
 
-    // 자원 종류
+    /// <summary>
+    /// 자원 종류
+    /// </summary>
+    // 자원 추가할때마다 Item의 전역변수 Count 수정하기
     enum Resource
     {
         Hydrogen,         // 수소
         Nitrogen,         // 질소
         Oxygen,           // 산소
+
         Peroxide,         // 퍼옥사이드
         Hydrazine,        // 하이드라진
         Epsilon,          // 엑실론
+
         Epsilon_crystal,  // 엑실론-크리스탈
-                             
-        Seed,             // 씨앗
         Water,            // 물
+
+        Seed,             // 씨앗
                              
         Chrono            // 크로노
     }
