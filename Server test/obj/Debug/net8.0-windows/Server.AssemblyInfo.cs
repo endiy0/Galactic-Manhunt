589414a7c03d68682c8efed877280bd60b3acdf0
DH.L <meozigoon@gmail.com>
2025-03-01 23:13:17 +0900

    v0.0.2-c
    TaskSelection.cs : button1_Click 가독성 향상

--- Server test/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ Server test/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbb699b2d4737384c573a7d13d5ac44448025cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069ff946c49afd6c2183d9442b0ac619ac78f910")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
