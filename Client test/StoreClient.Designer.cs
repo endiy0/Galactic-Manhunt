6eeb382856287c8c8b6a218337aca4c3c3fd8504
DH.L <meozigoon@gmail.com>
2025-03-02 03:35:36 +0900

    v0.0.2-c
    StoreClient.Designer.cs: Store UI made

--- Client test/StoreClient.Designer.cs
+++ Client test/StoreClient.Designer.cs
@@ -31,7 +31,15 @@
             dataGridView1 = new DataGridView();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView2 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
@@ -39,11 +47,11 @@
             dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Location = new Point(13, 54);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.Size = new Size(503, 538);
             dataGridView1.TabIndex = 0;
             // 
             // Column1
@@ -51,26 +59,99 @@
             Column1.HeaderText = "아이템";
             Column1.MinimumWidth = 8;
             Column1.Name = "Column1";
-            Column1.Width = 150;
+            Column1.Width = 250;
             // 
             // Column2
             // 
             Column2.HeaderText = "가격";
             Column2.MinimumWidth = 8;
             Column2.Name = "Column2";
-            Column2.Width = 150;
+            Column2.Width = 250;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 10F);
+            button1.Location = new Point(13, 598);
+            button1.Name = "button1";
+            button1.Size = new Size(503, 55);
+            button1.TabIndex = 1;
+            button1.Text = "구매";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 10F);
+            button2.Location = new Point(532, 598);
+            button2.Name = "button2";
+            button2.Size = new Size(503, 55);
+            button2.TabIndex = 3;
+            button2.Text = "구매";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2 });
+            dataGridView2.Location = new Point(532, 54);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.Size = new Size(503, 538);
+            dataGridView2.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "능력";
+            dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "가격";
+            dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.Width = 250;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 13F);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 36);
+            label1.TabIndex = 4;
+            label1.Text = "보유 화폐 :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 13F);
+            label2.Location = new Point(152, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 36);
+            label2.TabIndex = 5;
+            label2.Text = "(금액)";
             // 
             // StoreClient
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(615, 352);
+            ClientSize = new Size(1047, 666);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(dataGridView2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
-            Margin = new Padding(2);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "StoreClient";
             Text = "상점";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -78,5 +159,12 @@
         private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private Label label1;
+        private Label label2;
     }
 }
