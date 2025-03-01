1571db875c1a5f6eb57712480c7b20e07030c4ac
endiy0 <steve060401@gmail.com>
2025-03-02 00:43:41 +0900

    v0.0.2-c
    Sailor.cs : Work Added

--- Server test/Sailor.cs
+++ Server test/Sailor.cs
@@ -13,12 +13,14 @@ namespace Server_test
         public double EatInDay; // 식사량
         public bool isDisable; // 행동 불능 상태: True = 행동 불능, False = 정상
         public Inventory inventory;
-        public Sailor(SailorType T, double EatINDAY, double Maxinventory)
+        public Work work;
+        public Sailor(SailorType T, double EatINDAY, double Maxinventory, Work work)
         {
             inventory = new Inventory(Maxinventory, 0);
             type = T;
             EatInDay = EatINDAY;
             isDisable = false;
+            this.work = work;
         }
 
         public SailorType sailorType
