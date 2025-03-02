7788652397a9d09d0c0edb14af95b66ac0214a2e
DH.L <meozigoon@gmail.com>
2025-03-02 09:50:14 +0900

    v0..0.2-c
    ItemUse.cs: TODO added

--- Client test/ItemUse.cs
+++ Client test/ItemUse.cs
@@ -18,5 +18,9 @@ namespace Client_test
             InitializeComponent();
             this.form = form;
         }
+
+        // TODO: 아아템 사용 구현
+        // TODO: Enter 누르면 아이템 사용, 사용 시 확인칭으로 사용 확인
+
     }
 }
