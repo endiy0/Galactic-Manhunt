e7b8e487c961c054153cdc194c749e940f774dde
DH.L <meozigoon@gmail.com>
2025-03-02 09:48:18 +0900

    v0.0.2-c
    ItemUse.Designer.cs: UI made

--- Client test/ItemUse.Designer.cs
+++ Client test/ItemUse.Designer.cs
@@ -28,18 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button2 = new Button();
+            dataGridView2 = new DataGridView();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // Form6
+            // button2
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            button2.Font = new Font("맑은 고딕", 10F);
+            button2.Location = new Point(532, 556);
+            button2.Name = "button2";
+            button2.Size = new Size(503, 55);
+            button2.TabIndex = 7;
+            button2.Text = "사용";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2 });
+            dataGridView2.Location = new Point(532, 12);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.Size = new Size(503, 538);
+            dataGridView2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 10F);
+            button1.Location = new Point(12, 556);
+            button1.Name = "button1";
+            button1.Size = new Size(503, 55);
+            button1.TabIndex = 5;
+            button1.Text = "사용";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(503, 538);
+            dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "아이템";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.Width = 250;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "개수";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "능력";
+            dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.Width = 250;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "개수";
+            dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.Width = 250;
+            // 
+            // ItemUse
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form6";
+            ClientSize = new Size(1050, 624);
+            Controls.Add(button2);
+            Controls.Add(dataGridView2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "ItemUse";
             Text = "아이템 사용";
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button2;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
