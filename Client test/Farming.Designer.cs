999588499ed4199b2f1471fb266c77c6ca1bd550
DH.L <meozigoon@gmail.com>
2025-03-08 20:18:08 +0900

    v0.0.2-c
    Farming.cs: UI edited

--- Client test/Farming.Designer.cs
+++ Client test/Farming.Designer.cs
@@ -28,66 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             textBox1 = new TextBox();
             button1 = new Button();
             groupBox1 = new GroupBox();
             label4 = new Label();
             groupBox2 = new GroupBox();
-            textBox2 = new TextBox();
-            label5 = new Label();
             button2 = new Button();
+            label5 = new Label();
+            textBox2 = new TextBox();
             groupBox3 = new GroupBox();
-            label6 = new Label();
             button3 = new Button();
+            label6 = new Label();
             pictureBox1 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(77, 15);
-            label1.TabIndex = 0;
-            label1.Text = "씨앗량: 00kg";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(95, 9);
-            label2.Name = "label2";
-            label2.Size = new Size(133, 15);
-            label2.TabIndex = 1;
-            label2.Text = "현재 심은 씨앗량: 00kg";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(234, 9);
-            label3.Name = "label3";
-            label3.Size = new Size(145, 15);
-            label3.TabIndex = 2;
-            label3.Text = "수확 가능한 작물량: 00kg";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(6, 22);
+            textBox1.Location = new Point(9, 37);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(141, 31);
             textBox1.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(138, 22);
+            button1.Location = new Point(197, 37);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(107, 38);
             button1.TabIndex = 4;
             button1.Text = "심기";
             button1.UseVisualStyleBackColor = true;
@@ -97,9 +77,11 @@
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(textBox1);
-            groupBox1.Location = new Point(12, 27);
+            groupBox1.Location = new Point(12, 89);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(216, 51);
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(311, 85);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "씨앗 심기";
@@ -107,9 +89,10 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(112, 25);
+            label4.Location = new Point(157, 43);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(20, 15);
+            label4.Size = new Size(32, 25);
             label4.TabIndex = 4;
             label4.Text = "kg";
             // 
@@ -118,89 +101,149 @@
             groupBox2.Controls.Add(button2);
             groupBox2.Controls.Add(label5);
             groupBox2.Controls.Add(textBox2);
-            groupBox2.Location = new Point(231, 27);
+            groupBox2.Location = new Point(331, 89);
+            groupBox2.Margin = new Padding(4, 5, 4, 5);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(216, 51);
+            groupBox2.Padding = new Padding(4, 5, 4, 5);
+            groupBox2.Size = new Size(305, 85);
             groupBox2.TabIndex = 6;
             groupBox2.TabStop = false;
             groupBox2.Text = "물 주기";
             // 
-            // textBox2
+            // button2
             // 
-            textBox2.Location = new Point(6, 22);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 0;
+            button2.Location = new Point(190, 37);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 38);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(112, 25);
+            label5.Location = new Point(158, 40);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(20, 15);
+            label5.Size = new Size(32, 25);
             label5.TabIndex = 1;
             label5.Text = "kg";
             // 
-            // button2
+            // textBox2
             // 
-            button2.Location = new Point(138, 22);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 2;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            textBox2.Location = new Point(9, 37);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(141, 31);
+            textBox2.TabIndex = 0;
             // 
             // groupBox3
             // 
             groupBox3.Controls.Add(button3);
             groupBox3.Controls.Add(label6);
-            groupBox3.Location = new Point(12, 84);
+            groupBox3.Location = new Point(12, 184);
+            groupBox3.Margin = new Padding(4, 5, 4, 5);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(235, 47);
+            groupBox3.Padding = new Padding(4, 5, 4, 5);
+            groupBox3.Size = new Size(336, 107);
             groupBox3.TabIndex = 7;
             groupBox3.TabStop = false;
             groupBox3.Text = "수확하기";
             // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(6, 19);
-            label6.Name = "label6";
-            label6.Size = new Size(145, 15);
-            label6.TabIndex = 0;
-            label6.Text = "수확 가능한 작물량: 00kg";
-            // 
             // button3
             // 
-            button3.Location = new Point(157, 15);
+            button3.Location = new Point(197, 62);
+            button3.Margin = new Padding(4, 5, 4, 5);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(131, 38);
             button3.TabIndex = 1;
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(8, 29);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(218, 25);
+            label6.TabIndex = 0;
+            label6.Text = "수확 가능한 작물량: 00kg";
+            // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(253, 84);
+            pictureBox1.Image = Properties.Resources.Seed_Icon;
+            pictureBox1.Location = new Point(356, 193);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(194, 50);
+            pictureBox1.Size = new Size(280, 98);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridView1.Enabled = false;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(624, 69);
+            dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = null;
+            Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            Column1.HeaderText = "씨앗량";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 207;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            Column2.HeaderText = "심은 씨앗량";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 207;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.Format = "N2";
+            dataGridViewCellStyle3.NullValue = null;
+            Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            Column3.HeaderText = "수확 가능량";
+            Column3.MinimumWidth = 8;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 207;
+            // 
             // Farming
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(459, 143);
+            ClientSize = new Size(644, 302);
+            Controls.Add(dataGridView1);
             Controls.Add(pictureBox1);
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Margin = new Padding(1, 1, 1, 1);
+            Margin = new Padding(1, 2, 1, 2);
             Name = "Farming";
             Text = "농사";
             FormClosing += Farming_FormClosing;
@@ -211,15 +254,11 @@
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private Label label3;
         private TextBox textBox1;
         private Button button1;
         private GroupBox groupBox1;
@@ -232,5 +271,9 @@
         private Button button3;
         private Label label6;
         private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
     }
 }
