51f5acf41c4750bc6bbb70d1728f4541b1cd9f41
DH.L <meozigoon@gmail.com>
2025-03-08 10:08:08 +0900

    v0.0.2-c
    ChatClient.cs: comment edited

--- Client test/ChatClient.cs
+++ Client test/ChatClient.cs
@@ -341,18 +341,18 @@ namespace Client_test
     enum AbilityType
     {
         // 경찰
-        dark_under_the_lamp,   // 등잔 밑이 어둡다
-        galaxy_travel,         // 은하 탐방
-        planet_travel,         // 행성 탐방
-        stun,                  // 스턴
+        dark_under_the_lamp,  // 등잔 밑이 어둡다
+        galaxy_travel,        // 은하 탐방
+        planet_travel,        // 행성 탐방
+        stun,                 // 스턴
         handcuff,             // 수갑
-        team_identify,       // 팀 식별
+        team_identify,        // 팀 식별
 
         // 도둑                  
-        get_fuel,              // 겟 퓨얼
+        get_fuel,             // 겟 퓨얼
         fuel_changer,         // 연료 교환권
-        fuel_compressor,       // 연료 압축기
-        stun_remover,          // 스턴 제거기
+        fuel_compressor,      // 연료 압축기
+        stun_remover,         // 스턴 제거기
 
         // 공통                  
         store_growth          // 저장량 증가
