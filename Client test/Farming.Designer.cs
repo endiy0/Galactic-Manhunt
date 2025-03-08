59a81a8a1909652beef329fe8627d047b1702bf6
applejuwon <juwon02261@gmail.com>
2025-03-08 19:49:51 +0900

    v0.0.3-a
    Farming.cs: UI edited

--- Client test/Farming.Designer.cs
+++ Client test/Farming.Designer.cs
@@ -28,21 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            button2 = new Button();
+            groupBox3 = new GroupBox();
+            label6 = new Label();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 15);
+            label1.TabIndex = 0;
+            label1.Text = "씨앗량: 00kg";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(95, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(133, 15);
+            label2.TabIndex = 1;
+            label2.Text = "현재 심은 씨앗량: 00kg";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(234, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(145, 15);
+            label3.TabIndex = 2;
+            label3.Text = "수확 가능한 작물량: 00kg";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(138, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "심기";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 27);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(216, 51);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "씨앗 심기";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(112, 25);
+            label4.Name = "label4";
+            label4.Size = new Size(20, 15);
+            label4.TabIndex = 4;
+            label4.Text = "kg";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Location = new Point(231, 27);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(216, 51);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "물 주기";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(6, 22);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(112, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(20, 15);
+            label5.TabIndex = 1;
+            label5.Text = "kg";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(138, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(label6);
+            groupBox3.Location = new Point(12, 84);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(235, 47);
+            groupBox3.TabIndex = 7;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "수확하기";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(145, 15);
+            label6.TabIndex = 0;
+            label6.Text = "수확 가능한 작물량: 00kg";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(157, 15);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 1;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(253, 84);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(194, 50);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
             // 
             // Farming
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(615, 352);
+            ClientSize = new Size(459, 143);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Margin = new Padding(2);
+            Margin = new Padding(1, 1, 1, 1);
             Name = "Farming";
             Text = "농사";
             FormClosing += Farming_FormClosing;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button1;
+        private GroupBox groupBox1;
+        private Label label4;
+        private GroupBox groupBox2;
+        private Button button2;
+        private Label label5;
+        private TextBox textBox2;
+        private GroupBox groupBox3;
+        private Button button3;
+        private Label label6;
+        private PictureBox pictureBox1;
     }
 }
