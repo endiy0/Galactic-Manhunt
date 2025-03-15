af95b48eb65fc893d9ac8579c5617928a1e71c8a
blackzebra500 <a01098989661@gmail.com>
2025-03-15 16:59:02 +0900

    v0.0.3 - a
    FuelSynthesis.cs : creator created and remove function created

--- Server test/FuelSynthesis.cs
+++ Server test/FuelSynthesis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,12 @@ namespace Server_test
 {
     internal class FuelSynthesis
     {
-        Item return_fuel = null;
-        ShipType shipType;
-
         // 엑실론 제외 연료 합성
+
+        List<Item>return_list = null;
         public FuelSynthesis(Item item1, Item item2, ShipType type)
         {
-            shipType = type;
+            Item return_fuel;
 
             // 근데 여기서 문제는 이 순서로 넘겨야함
 
@@ -25,16 +25,46 @@ namespace Server_test
                 return_fuel = new Item(Resource.Hydrazine, item1.mass);      // 일단 결합은 질량비 1:2
             }
 
-            else if (item1.GetItemType() == Resource.Oxygen && item2.GetItemType() == Resource.Hydrogen)
+            /*else if (item1.GetItemType() == Resource.Oxygen && item2.GetItemType() == Resource.Hydrogen)
             {
                 return_fuel = new Item(Resource.Peroxide, item1.mass);       // 여기는 1:1
+            }*/
+            else
+            {
+                return_fuel = new Item(Resource.Peroxide, item1.mass);
             }
+
+            return_list.Add(return_fuel);
         }
         
         // 엑실론 합성시
         public FuelSynthesis(Item item, ShipType type)
         {
-            shipType = type;
+            Item return_fuel;
+            
+            if(type == ShipType.galaxy_moving_ship)
+            {
+                return_fuel = new Item(Resource.Epsilon, item.mass / 3);
+            }
+            else
+            {
+                return_fuel = new Item(Resource.Epsilon, item.mass / 2);
+            }
+
+            return_list.Add(return_fuel);
+        }
+
+        public List<Item> Return_Fuel()
+        {
+            List<Item> list = return_list;
+            return_list.Clear();         // 비워주기 매번 초기화해줘야 다른 사람 쓸때 오류 없어요
+            return list;
+        }
+
+        // 합성 창 닫을 때 list 비워주는거
+        public void Remove()
+        {
+            return_list.Clear();
         }
     }
 }
